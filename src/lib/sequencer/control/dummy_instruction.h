31aa5d76fe15a7f4df693570df3bc3a590616c60
Walter Van Herck <walter.vanherck@iter.org>
2023-07-13 09:07:42 +0000

    Basic folder structure

---
+++ src/lib/sequencer/control/dummy_instruction.h
@@ -0,0 +1,51 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Sequencer for operational procedures
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+******************************************************************************/
+
+#ifndef SUP_SEQUENCER_PLUGIN_CONTROL_DUMMY_INSTRUCTION_H_
+#define SUP_SEQUENCER_PLUGIN_CONTROL_DUMMY_INSTRUCTION_H_
+
+#include <sup/sequencer/instruction.h>
+
+namespace sup
+{
+namespace sequencer
+{
+
+/**
+ * @brief Does nothing.
+ */
+class DummyInstruction : public Instruction
+{
+public:
+  DummyInstruction();
+  ~DummyInstruction() override;
+
+  static const std::string Type;
+
+private:
+  ExecutionStatus ExecuteSingleImpl(UserInterface& ui, Workspace& ws) override;
+};
+
+}  // namespace sequencer
+
+}  // namespace sup
+
+#endif  // SUP_SEQUENCER_PLUGIN_CONTROL_DUMMY_INSTRUCTION_H_
