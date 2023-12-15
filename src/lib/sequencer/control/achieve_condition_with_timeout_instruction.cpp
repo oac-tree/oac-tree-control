07f987a96fe0ab90c1524672c7d254ceb449042f
Walter Van Herck <walter.vanherck@iter.org>
2023-12-15 16:07:36 +0000

    Add AchieveConditionWithTimeout (not fully satisfying yet)

---
+++ src/lib/sequencer/control/achieve_condition_with_timeout_instruction.cpp
@@ -0,0 +1,133 @@
+/******************************************************************************
+* $HeadURL: $
+* $Id: $
+*
+* Project       : Supervision and Automation - Sequencer
+*
+* Description   : SUP sequencer control plugin
+*
+* Author        : Walter Van Herck (IO)
+*
+* Copyright (c) : 2010-2023 ITER Organization,
+*                 CS 90 046
+*                 13067 St. Paul-lez-Durance Cedex
+*                 France
+*
+* This file is part of ITER CODAC software.
+* For the terms and conditions of redistribution or use of this software
+* refer to the file ITER-LICENSE.TXT located in the top level directory
+* of the distribution package.
+******************************************************************************/
+
+#include "achieve_condition_with_timeout_instruction.h"
+
+#include "wrapped_user_interface.h"
+
+#include <sup/sequencer/instruction_registry.h>
+#include <sup/sequencer/instruction_utils.h>
+
+namespace sup {
+
+namespace sequencer {
+
+const std::string AchieveConditionWithTimeoutInstruction::Type = "AchieveConditionWithTimeout";
+static bool _wait_for_condition_initialised_flag =
+  RegisterGlobalInstruction<AchieveConditionWithTimeoutInstruction>();
+
+const std::string TIMEOUT_ATTR_NAME = "timeout";
+const std::string VARNAMES_ATTRIBUTE_NAME = "varNames";
+
+const std::string LOG_MESSAGE_PREFIX =
+  "Forwarded log message from internal instruction of AchieveConditionWithTimeout: ";
+
+AchieveConditionWithTimeoutInstruction::AchieveConditionWithTimeoutInstruction()
+  : CompoundInstruction(Type)
+  , m_internal_instruction_tree{}
+  , m_instr_manager{}
+{
+  AddAttributeDefinition(VARNAMES_ATTRIBUTE_NAME).SetMandatory();
+  AddAttributeDefinition(TIMEOUT_ATTR_NAME, sup::dto::Float64Type)
+    .SetCategory(AttributeCategory::kBoth).SetMandatory();
+}
+
+AchieveConditionWithTimeoutInstruction::~AchieveConditionWithTimeoutInstruction() = default;
+
+void AchieveConditionWithTimeoutInstruction::SetupImpl(const Procedure& proc)
+{
+  auto instr_tree = CreateWrappedInstructionTree();
+  std::swap(m_internal_instruction_tree, instr_tree);
+  m_internal_instruction_tree->Setup(proc);
+}
+
+ExecutionStatus AchieveConditionWithTimeoutInstruction::ExecuteSingleImpl(UserInterface& ui, Workspace& ws)
+{
+  m_instr_manager.SetContext(ui);
+  auto& wrapped_ui = m_instr_manager.GetWrappedUI(ui, LOG_MESSAGE_PREFIX);
+  m_internal_instruction_tree->ExecuteSingle(wrapped_ui, ws);
+  return m_internal_instruction_tree->GetStatus();
+}
+
+void AchieveConditionWithTimeoutInstruction::HaltImpl()
+{
+  if (m_internal_instruction_tree)
+  {
+    m_internal_instruction_tree->Halt();
+  }
+}
+
+void AchieveConditionWithTimeoutInstruction::ResetHook()
+{
+  if (m_internal_instruction_tree)
+  {
+    m_internal_instruction_tree->Reset();
+  }
+}
+
+std::vector<const Instruction*> AchieveConditionWithTimeoutInstruction::NextInstructionsImpl() const
+{
+  return FilterNextInstructions(*this, m_internal_instruction_tree.get());
+}
+
+std::unique_ptr<Instruction> AchieveConditionWithTimeoutInstruction::CreateWrappedInstructionTree()
+{
+  m_instr_manager.ClearWrappers();
+  auto children = ChildInstructions();
+  if (children.size() != 2)
+  {
+    std::string error_message = InstructionErrorProlog(*this) +
+      "This compound instruction requires exactly two child instructions";
+    throw InstructionSetupException(error_message);
+  }
+
+  // Wrapped condition
+  auto cond_wrapper = m_instr_manager.CreateInstructionWrapper(*children[0]);
+
+  // Wrapped action
+  auto action_wrapper = m_instr_manager.CreateInstructionWrapper(*children[1]);
+  auto force_success = GlobalInstructionRegistry().Create("ForceSuccess");
+  force_success->InsertInstruction(std::move(action_wrapper), 0);
+
+  // Wait for condition with another wrapped condition
+  auto cond_wrapper_2 = m_instr_manager.CreateInstructionWrapper(*children[0]);
+  auto wait_with_timeout = GlobalInstructionRegistry().Create("WaitForCondition");
+  wait_with_timeout->AddAttribute(VARNAMES_ATTRIBUTE_NAME,
+                                  GetAttributeString(VARNAMES_ATTRIBUTE_NAME));
+  wait_with_timeout->AddAttribute(TIMEOUT_ATTR_NAME, GetAttributeString(TIMEOUT_ATTR_NAME));
+  wait_with_timeout->InsertInstruction(std::move(cond_wrapper_2), 0);
+
+  // Sequence combining action and wait with timeout
+  auto sequence = GlobalInstructionRegistry().Create("Sequence");
+  sequence->InsertInstruction(std::move(force_success), 0);
+  sequence->InsertInstruction(std::move(wait_with_timeout), 1);
+
+  // Fallback combining the condition and the sequence
+  auto fallback = GlobalInstructionRegistry().Create("Fallback");
+  fallback->InsertInstruction(std::move(cond_wrapper), 0);
+  fallback->InsertInstruction(std::move(sequence), 1);
+
+  return fallback;
+}
+
+} // namespace sequencer
+
+} // namespace sup
---
+++ src/lib/sequencer/control/achieve_condition_with_timeout_instruction.h
@@ -0,0 +1,70 @@
+/******************************************************************************
+* $HeadURL: $
+* $Id: $
+*
+* Project       : Supervision and Automation - Sequencer
+*
+* Description   : SUP sequencer control plugin
+*
+* Author        : Walter Van Herck (IO)
+*
+* Copyright (c) : 2010-2023 ITER Organization,
+*                 CS 90 046
+*                 13067 St. Paul-lez-Durance Cedex
+*                 France
+*
+* This file is part of ITER CODAC software.
+* For the terms and conditions of redistribution or use of this software
+* refer to the file ITER-LICENSE.TXT located in the top level directory
+* of the distribution package.
+******************************************************************************/
+
+#ifndef SUP_SEQUENCER_PLUGIN_CONTROL_ACHIEVE_CONDITION_WITH_TIMEOUT_INSTRUCTION_H_
+#define SUP_SEQUENCER_PLUGIN_CONTROL_ACHIEVE_CONDITION_WITH_TIMEOUT_INSTRUCTION_H_
+
+#include "wrapped_instruction_manager.h"
+
+#include <sup/sequencer/compound_instruction.h>
+
+#include <memory>
+
+namespace sup
+{
+namespace sequencer
+{
+
+/**
+ * @brief Try to achieve a condition. If it is already satisfied, returns SUCCESS. Otherwise, if
+ * present, it executes an action to achieve the condition. If the condition still fails afterwards,
+ * the instruction exits with FAILURE. The second check is done using a timeout to allow a system
+ * to reach the condition.
+ *
+ * @details This compound instruction expects exactly two child instructions: the first one
+ * is the condition to achieve and the second one is the instruction (or tree)
+ * to execute when the condition is not (yet) satisfied.
+ */
+class AchieveConditionWithTimeoutInstruction : public CompoundInstruction
+{
+public:
+  AchieveConditionWithTimeoutInstruction();
+  ~AchieveConditionWithTimeoutInstruction() override;
+
+  static const std::string Type;
+
+private:
+  void SetupImpl(const Procedure& proc) override;
+  ExecutionStatus ExecuteSingleImpl(UserInterface& ui, Workspace& ws) override;
+  void HaltImpl() override;
+  void ResetHook() override;
+  std::vector<const Instruction*> NextInstructionsImpl() const override;
+  std::unique_ptr<Instruction> CreateWrappedInstructionTree();
+
+  std::unique_ptr<Instruction> m_internal_instruction_tree;
+  WrappedInstructionManager m_instr_manager;
+};
+
+}  // namespace sequencer
+
+}  // namespace sup
+
+#endif  // SUP_SEQUENCER_PLUGIN_CONTROL_ACHIEVE_CONDITION_WITH_TIMEOUT_INSTRUCTION_H_
